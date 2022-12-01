e8a6d222aa5daf27bfb057623c0d769761ae4e33
NiniaKutalia <114922116+NiniaKutalia@users.noreply.github.com>
2022-12-02 00:47:32 +0400

    styles

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,17 +1,89 @@
-let slideIndex = 1;
-showDivs(slideIndex);
+// let slideIndex = 1;
+// showDivs(slideIndex);
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
+// function plusDivs(n) {
+//   showDivs(slideIndex += n);
+// }
+
+// function showDivs(n) {
+//   let i;
+//   let x = document.getElementsByClassName("Slides");
+//   if (n > x.length) {slideIndex = 1}
+//   if (n < 1) {slideIndex = x.length}
+//   for (i = 0; i < x.length; i++) {
+//      x[i].style.display = "none";
+//   }
+//   x[slideIndex-1].style.display = "block";
+// }
+
+
+// GALLERY
+let arrowIconLeft = '<?xml version="1.0" ?><!DOCTYPE svg  PUBLIC \'-//W3C//DTD SVG 1.1//EN\'  \'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\'><svg height="50px" id="Layer_1" style="enable-background:new 0 0 50 50;" version="1.1" viewBox="0 0 512 512" width="50px" color="#fff" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><polygon points="352,115.4 331.3,96 160,256 331.3,416 352,396.7 201.5,256 "/></svg>';
+
+let slides = document.querySelectorAll('.slide-ana div');
+let slideSayisi = slides.length;
+
+
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+prev.innerHTML = arrowIconLeft;
+next.innerHTML = arrowIconLeft;
+next.querySelector('svg').style.transform = 'rotate(180deg)';
+
+
+for (let index = 0; index < slides.length; index++) {
+    const element = slides[index];
+    element.style.transform = "translateX("+100*(index)+"%)";
+}
+let loop = 0 + 1000*slideSayisi;
+
+function goNext(){
+    loop++;
+            for (let index = 0; index < slides.length; index++) {
+                const element = slides[index];
+                element.style.transform = "translateX("+100*(index-loop%slideSayisi)+"%)";
+            }
 }
 
-function showDivs(n) {
-  let i;
-  let x = document.getElementsByClassName("Slides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length}
-  for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";  
+function goPrev(){
+    loop--;
+            for (let index = 0; index < slides.length; index++) {
+                const element = slides[index];
+                element.style.transform = "translateX("+100*(index-loop%slideSayisi)+"%)";
+            }
+}
+
+next.addEventListener('click',goNext);
+prev.addEventListener('click',goPrev);
+document.addEventListener('keydown',function(e){
+    if(e.code === 'ArrowRight'){
+        goNext();
+    }else if(e.code === 'ArrowLeft'){
+        goPrev();
+    }
+});
+
+
+
+
+
+// BACKGROUND GRADIENT
+
+const colors = ['', 'lightCyan', 'darkSkyBlue', 'aquamarine', 'electricBlue']
+
+const sections = [...document.querySelectorAll('section')]
+
+window.addEventListener('scroll', function () {
+
+  const scrollFromTop = window.pageYOffset
+
+  for (let i = 0; sections.length > i; i++) {
+
+    if (scrollFromTop <= sections[i].offsetTop) {
+      document.body.className = colors[i] 
+      break
+    } 
+
   }
-  x[slideIndex-1].style.display = "block";  
-}
+
+})
