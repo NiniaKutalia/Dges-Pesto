fcfa0d4e02cd233880a7a6fa8c657a39a1b4ccc0
NiniaKutalia <114922116+NiniaKutalia@users.noreply.github.com>
2022-12-02 19:13:42 +0400

    blog, products, recipe

--- js/aboutus.js
+++ js/aboutus.js
@@ -1,20 +1,3 @@
-// let slideIndex = 1;
-// showDivs(slideIndex);
-
-// function plusDivs(n) {
-//   showDivs(slideIndex += n);
-// }
-
-// function showDivs(n) {
-//   let i;
-//   let x = document.getElementsByClassName("Slides");
-//   if (n > x.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = x.length}
-//   for (i = 0; i < x.length; i++) {
-//      x[i].style.display = "none";
-//   }
-//   x[slideIndex-1].style.display = "block";
-// }
 
 
 // GALLERY
@@ -62,6 +45,10 @@ document.addEventListener('keydown',function(e){
         goPrev();
     }
 });
+
+
+// BLOG
+
 
 
 
